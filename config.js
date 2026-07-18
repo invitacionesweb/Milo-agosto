92a60df4631e3f17a49c5fbb2a0229225f1d8e1f
invitacionesweb <invitacionesweb.info@gmail.com>
2026-07-17 21:15:22 -0300

    A
    A

---
+++ config.js
@@ -0,0 +1,41 @@
+// =============================================
+//  CONFIGURACIÓN DE LA INVITACIÓN - ROMA
+//  Editá solo este archivo para cada cliente
+// =============================================
+
+const CONFIG = {
+
+  // --- DATOS DEL EVENTO ---
+  nombreFestejado: "Milo",
+  subtitulo: "Te esperamos",
+
+  // --- FECHA PARA EL CONTADOR (ISO 8601) ---
+  fechaContador: "2026-08-15T23:59:59",
+
+  // --- UBICACIÓN ---
+  textoUbicacion: "Te esperamos para celebrar mi bautismo a las 16:00 hs en Parroquia San Gabriel.",
+textoUbicacion2: "Te esperamos para festejar mis tres añitos<br>de 14 a 18:30 hs<br>en Nijash Kids<br>Hace click para saber como llegar.",
+  linkMapa: "https://www.google.com/maps/dir//Nijash+Kids,+Mir%C3%B3+3782+B1753FET,+B1753FET+Villa+Luzuriaga,+Provincia+de+Buenos+Aires/@-34.6741352,-58.5908315,17z/data=!4m17!1m7!3m6!1s0x95bcc71b6f8e65db:0x5771ac231e4561a2!2sNijash+Kids!8m2!3d-34.6741352!4d-58.5882566!16s%2Fg%2F11v3zvfvw5!4m8!1m0!1m5!1m1!1s0x95bcc71b6f8e65db:0x5771ac231e4561a2!2m2!1d-58.5883208!2d-34.6740869!3e0?entry=ttu&g_ep=EgoyMDI2MDcxNS4wIKXMDSoASAFQAw%3D%3D",
+  linkAgendar: "",
+
+  // --- FORMULARIO ---
+  textoForm: "Tu presencia es lo más importante",
+  textoFormSub: "No olvides confirmar asistencia con mis papis antes del 12 de Agosto.",
+  maxInvitados: 5,
+  scriptFormUrl: "",
+
+  // --- AUDIO ---
+  archivoAudio: "./ABCD.mp3",
+
+  // --- COLORES ---
+  colores: {
+    color0: "#ffffff",
+    color1: "#E4C7C5",
+    color2: "#f8f3ed",
+    color3: "#E4C7C5",
+  },
+
+  // --- TÍTULO DE LA PESTAÑA ---
+  tituloTab: "Milo",
+  
+};
